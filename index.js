bda41092b77f9567776479afd36cabf299994fcc
niz.gabriel <bielnizps@gmail.com>
2024-02-02 21:09:45 -0300

    Projeto-Landing-Page

---
+++ index.js
@@ -0,0 +1,18 @@
+var setaDireita = window.document.getElementById("seta-direita")
+var Leonardo = window.document.getElementById("Leonardo")
+var Bruna = window.document.getElementById("Bruna")
+var Samantha = window.document.getElementById("Samantha")
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+
+function RolarParaDireita() {
+    Leonardo.style ="display:none"
+    Bruna.style ="display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex; margin-top:55px"    
+}
+function RolarParaEsquerda(){
+    Leonardo.style="display:flex"
+    Bruna.style="display:none"
+    setaDireita.style="display:flex; margin-top:55px"  
+    setaEsquerda.style = "display:none"
+}
